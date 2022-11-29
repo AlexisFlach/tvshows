6746da55e66200cfad249fa8de4556e0ec85a010
AlexisFlach <Alexis.Flach87@gmail.com>
2022-11-29 10:46:49 +0100

    add env file

--- src/services/api/index.tsx
+++ src/services/api/index.tsx
@@ -1,25 +1,14 @@
 import axios from 'axios';
-
-// export const ApiService = () => {
-
-//     const apiService = {
-//         getShows: async () => {
-//             const shows = await axios.get('http://api.tvmaze.com/shows')
-//             return shows;
-//         }
-//     }
-//     return { apiService };
-// }
-
+const API_URL = process.env.REACT_APP_API_URL;
 export class ApiService {
 
     static getShows = async () => {
-        const data = await axios.get('http://api.tvmaze.com/shows')
+        const data = await axios.get(`${API_URL}/shows`)
         return data;
     }
 
     static getShow = async (id: number) => {
-        const data = await axios.get(`http://api.tvmaze.com/shows/${id}`)
+        const data = await axios.get(`${API_URL}/shows/${id}`)
         return data;
     }
 
